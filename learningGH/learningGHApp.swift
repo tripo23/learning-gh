b1733f05921d316d1a8f41d1e6d9dd76b5481b38
Nicolas Tripodi <tripodi.nicolas@gmail.com>
2021-06-19 15:46:51 -0300

    Initial Commit

---
+++ learningGH/learningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learningGHApp.swift
+//  learningGH
+//
+//  Created by Nicolás Trípodi on 19/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct learningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
